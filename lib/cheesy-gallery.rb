272557ba5c7bb501bddab86ef481a6fd5df19135
David Schmitt <david@black.co.at>
2020-01-04 16:27:15 +0000

    Add first file and jekyll dependency

---
+++ lib/cheesy-gallery.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "jekyll"
+require "fileutils"
+require "cheesy-gallery/generator"
+
+module CheesyGallery
+  # autoload :Const, "cheesy-gallery/const"
+end
+
+# Liquid::Template.register_tag "gallery-link", CheesyGallery::GalleryLink
