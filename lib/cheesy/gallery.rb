24eccb40d8a6b6f83e3a7643820410911d78de10
David Schmitt <david@black.co.at>
2020-01-04 16:04:04 +0000

    `bundle gem cheesy-gallery`

---
+++ lib/cheesy/gallery.rb
@@ -0,0 +1,8 @@
+require "cheesy/gallery/version"
+
+module Cheesy
+  module Gallery
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/cheesy/gallery/version.rb
@@ -0,0 +1,5 @@
+module Cheesy
+  module Gallery
+    VERSION = "0.1.0"
+  end
+end
