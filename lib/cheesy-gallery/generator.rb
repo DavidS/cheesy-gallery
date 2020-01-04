4ab7aeb171a340df5330f180c4cbfa854fa3ef61
David Schmitt <david@black.co.at>
2020-01-04 17:55:37 +0000

    Add docstring for Generator
    thanks, rubocop!

--- lib/cheesy-gallery/generator.rb
+++ lib/cheesy-gallery/generator.rb
@@ -3,6 +3,7 @@
 require 'jekyll'
 
 module CheesyGallery
+  # The generator modifies the `site` data structure to contain all data necessary by the layouts and tags to render the galleries
   class Generator < Jekyll::Generator
     def generate(site)
       # read `_galleries`
