bea4ac8e1accb76701eb23a16844a3318dee9770
David Schmitt <david.schmitt@puppet.com>
2020-01-05 10:01:36 +0000

    Whitespace fix

--- lib/cheesy-gallery/utils.rb
+++ lib/cheesy-gallery/utils.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This Page subclass can be used to create new pages in the target site from scratch
-class CheesyGallery:: PageWithoutAFile < Jekyll::Page
+class CheesyGallery::PageWithoutAFile < Jekyll::Page
   def read_yaml(*)
     @data ||= {} # this is the correct side-effect of this method # rubocop:disable Naming/MemoizedInstanceVariableName
   end
