d95692f21c25fb677b7748a350afca55e6dcd584
David Schmitt <david@black.co.at>
2020-01-11 15:26:00 +0000

    Use the Collection and Document to build gallery
    This now creates an `index.html` for each gallery folder, even if there
    is no input file. A document for the gallery also has the list of images
    that are attached to that gallery as a property that can be accessed
    from Liquid.
    
    This makes the code a lot nicer and has the same functionality as before,
    only actually working: with a slight change to the gallery layout, it now
    displays images and fits into the layout of the theme!

--- lib/cheesy-gallery/generator.rb
+++ lib/cheesy-gallery/generator.rb
@@ -2,69 +2,33 @@
 
 require 'jekyll'
 require 'json'
-require 'cheesy-gallery/gallery_page'
+require 'cheesy-gallery/gallery_index'
 
 # The generator modifies the `site` data structure to contain all data necessary by the layouts and tags to render the galleries
 class CheesyGallery::Generator < Jekyll::Generator
   def generate(site)
     @site = site
-    # require 'pry'; binding.pry
+    collection = site.collections['galleries']
 
-    galleries = collect_galleries(File.join(site.source, '_galleries'), File.join(site.dest, 'galleries'))
+    # all galleries in the site
+    galleries = Set[*collection.entries.map { |e| File.dirname(e) }]
 
-    # debug_page = make_gallery_page('/source_path', '/target_path', '/file.html')
-    debug_page = make_gallery_page('nil', '', 'debug.html')
-    debug_page.content = JSON.pretty_generate(galleries)
-    site.pages << debug_page
+    # all galleries with an index.html
+    galleries_with_index = Set[*collection.entries.find_all { |e| e.end_with?('/index.html') }.map { |e| File.dirname(e) }]
 
-    galleries.each do |g|
-      site.pages << make_gallery_page(site.source, File.join('_galleries', g[:path]), 'index.html')
-    end
-  end
-
-  # source_dir: join(site.source, config[:gallery][:source] = '_galleries')
-  # dest_dir: join(site.dest, config[:gallery][:dest] = 'galleries' )
-  # next_dir: relative path within the gallery tree, used in recursion, leave `nil` for first level
-  def collect_galleries(source_dir, dest_dir, next_dir = nil)
-    if next_dir.nil?
-      current_dir = source_dir
-      next_dir = '/'
-    else
-      current_dir = File.join(source_dir, next_dir)
+    # fill in Documents for galleries that don't have an index.html
+    (galleries - galleries_with_index).each do |e|
+      doc = CheesyGallery::GalleryIndex.new(File.join('_galleries', e, 'index.html'), site: site, collection: collection)
+      doc.read
+      collection.docs << doc if site.unpublished || doc.published?
     end
 
-    (Dir.foreach(current_dir).collect do |entry|
-      # skip self and parent directory
-      next if ['.', '..'].include?(entry)
+    files_by_dirname = {}
+    collection.files.each { |e| (files_by_dirname[File.dirname(e.relative_path)] ||= []) << e }
 
-      path = File.join(next_dir, entry)
-
-      # files (as opposed to directories) will be handled later
-      source_path = File.join(source_dir, path)
-      next unless File.directory?(source_path)
-
-      dest_path = File.join(dest_dir, path)
-
-      collect_galleries(source_dir, dest_dir, path) + [
-        {
-          name: entry,
-          path: path,
-          source: source_path,
-          dest: dest_path,
-        },
-      ]
-    end || []).flatten.find_all { |f| !f.nil? }
-  end
-
-  def make_gallery_page(source_path, target_path, path)
-    CheesyGallery::GalleryPage.new(@site, @site.source, target_path, path).tap do |file|
-      file.data['layout'] ||= 'gallery'
-      file.data.merge!(
-        'sitemap' => false,
-        'source' => source_path,
-        'images' => %w[a b],
-      )
-      file.output
+    collection.docs.each do |doc|
+      # attach images
+      doc.data['images'] = files_by_dirname[File.dirname(doc.relative_path)]
     end
   end
 end
